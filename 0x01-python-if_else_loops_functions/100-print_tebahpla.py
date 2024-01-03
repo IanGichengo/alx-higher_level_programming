f35f0d020aef3c17c9a02c01464b2c81d252f1e9
IanGichengo <iangatimu6@gmail.com>
2024-01-03 13:24:46 -0500

    python_loops_functions

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-import string
-letters = []
-for i in range (len (string.ascii_lowercase) - 1, -1, -1):
-    if i % 2 == 0:
-        letters.append (string.ascii_lowercase [i].upper ())
-    else:
-        letters.append (string.ascii_lowercase [i])
-        result = "".join (letters)
-        print ("{}".format (result))
+for i in range(ord('z'), ord('A') - 1, -1):
+    print(chr(i) + chr(i - ord('A') + ord('a')), end='')
