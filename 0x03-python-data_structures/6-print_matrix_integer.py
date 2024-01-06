a2cb3070b221327279c7d57b716bbb74b0b4bdbc
IanGichengo <iangatimu6@gmail.com>
2024-01-06 05:36:30 -0500

    python_lists_tuples

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for h in range(len(matrix)):
-        for k in range(len(matrix[h])):
-            print("{:d}".format(matrix[h][k]), end="")
-            if k != (len(matrix[h]) - 1):
-                print(" ", end="")
-                print("")
+    for row in matrix:
+        for idx, num in enumerate(row):
+            if idx == len(row) - 1:
+                print("{:d}".format(num))
+            else:
+                print("{:d} ".format(num), end="")
+    if not matrix:
+        print("$")
