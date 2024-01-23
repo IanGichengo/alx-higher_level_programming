1912552212157591f3cc19a545d5707a9de52b80
IanGichengo <iangatimu6@gmail.com>
2024-01-23 11:10:31 -0500

    classes_objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" a class square with size details """
+
+
+class Square:
+
+    """ private attribute of class square """
+    def __init__(self, size):
+
+        """ the size of the square defined """
+        self.__size = size
