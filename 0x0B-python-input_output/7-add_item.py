7be436b3d961bfd9dec4582765728f1459b0ff9a
IanGichengo <iangatimu6@gmail.com>
2024-02-07 06:13:34 -0500

    input_output_in_python

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
+""" contains a script that adds all arguments """
 
-import json
+
 import sys
-from os import path
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+import json
+
+""" Import the custom functions from other files """
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-""" Filename for the JSON file """
 filename = "add_item.json"
 
-if not path.exists(filename):
-    save_to_json_file([], filename)
+"""Try to load the list from the file """
+try:
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
 
-my_list = load_from_json_file(filename)
+for arg in sys.argv[1:]:
+    my_list.append(arg)
 
-""" Append command line arguments to the list """
-my_list.extend(sys.argv[1:])
 
 save_to_json_file(my_list, filename)
