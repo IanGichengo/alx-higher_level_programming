6d7372a98deb26a6fce21e5fc75a1967613e79d4
IanGichengo <iangatimu6@gmail.com>
2024-02-07 06:30:37 -0500

    input_output_in_python

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,14 +5,8 @@
 def class_to_json(obj):
     """ obj  is an instance of a class dict contains attributes """
 
-    json_dict = {}
+    ian_dict = {}
+    for key, value in obj.__dict__.items():
+        ian_dict[key] = value
 
-    for attr_name in dir(obj):
-
-        if not attr_name.startswith("__"):
-            attr_value = getattr(obj, attr_name)
-            if isinstance(attr_value, (list, dict, str, int, bool)):
-                json_dict[attr_name] = attr_value
-                """ add the attribute to the dictionary """
-
-                return json_dict
+        return ian_dict
