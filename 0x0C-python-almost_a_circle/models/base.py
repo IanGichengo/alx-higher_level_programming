bdf28e1823029fb5958a7715f4cd8101ad8d3e67
IanGichengo <iangatimu6@gmail.com>
2024-02-16 07:54:39 -0500

    almost_a_circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,56 @@
 #!/usr/bin/python3
-""" the base for other classes in the project """
+""" base method """
+
+import json
 
 
 class Base:
-    """Base class for managing ids."""
-
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize Base class."""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        obj_list = []
+        if list_objs is not None:
+            for obj in list_objs:
+                obj_list.append(obj.to_dictionary())
+        with open(filename, "w") as file:
+            file.write(cls.to_json_string(obj_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            new_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            new_instance = cls(1)
+        new_instance.update(**dictionary)
+        return new_instance
+
+    @classmethod
+    def load_from_file(cls):
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as file:
+                data = cls.from_json_string(file.read())
+                return [cls.create(**d) for d in data]
+        except FileNotFoundError:
+            return []
