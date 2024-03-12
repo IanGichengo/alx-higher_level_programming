8d2ce4fdbc8403315af21991fc21faba37a44af5
IanGichengo <iangatimu6@gmail.com>
2024-03-12 15:45:22 -0400

    SQL_introduction

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 -- prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT FROM COLUMNS WHERE TABLE_SCHEMA='$1' AND TABLE_NAME='first_table';
+SHOW CREATE TABLE first_table;
