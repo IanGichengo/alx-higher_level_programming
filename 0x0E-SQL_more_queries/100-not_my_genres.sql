723b58eb30efb4ba3a2155c891f8cdab7ed8a65a
IanGichengo <iangatimu6@gmail.com>
2024-03-13 16:07:45 -0400

    SQL_queries

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -7,7 +7,11 @@
 
 SELECT tv_genres.name
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title != 'Dexter' OR tv_shows.title IS NULL
-ORDER BY tv_genres.name ASC;
+LEFT JOIN (
+    SELECT tv_show_genres.genre_id
+    FROM tv_show_genres
+    INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
+) AS linked_genres ON tv_genres.id = linked_genres.genre_id
+WHERE linked_genres.genre_id IS NULL
+ORDER BY tv_genres.name;
