7fb30d2d7f1252f8858e2d72ef12549eb00dc1c1
IanGichengo <iangatimu6@gmail.com>
2024-03-14 13:03:12 -0400

    SQL_queries

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -4,8 +4,8 @@
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title, SUM(rating) AS rating
-FROM hbtn_0d_tvshows_rate
-INNER JOIN tv_shows ON hbtn_0d_tvshows_rate.show_id = tv_shows.id
-GROUP BY tv_shows.title
-ORDER BY `rating sum` DESC;
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.id
+ORDER BY rating DESC;
