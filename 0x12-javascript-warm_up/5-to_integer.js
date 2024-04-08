1b46c7fc3ab7c4259236170e283b71a1cae308ef
IanGichengo <iangatimu6@gmail.com>
2024-04-08 16:31:50 -0400

    js_warm_up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const arg = process.argv[2]
-const num = Number.parseInt(arg)
+const arg = process.argv[2];
+const num = Number.parseInt(arg);
 
 if (Number.isNaN(num)) {
-  console.log('Not a number')
-} else { console.log('My number: ' + num) }
+  console.log('Not a number');
+} else { console.log('My number: ' + num); }
