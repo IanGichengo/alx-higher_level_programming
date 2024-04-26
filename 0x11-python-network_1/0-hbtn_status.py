bbb3c96509568ef69cdcbbf4e6818281679e6878
IanGichengo <iangatimu6@gmail.com>
2024-04-26 07:27:34 -0400

    python_network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,15 +2,11 @@
 # Import the required module
 import urllib.request
 
-# Define the URL
 url = "https://alx-intranet.hbtn.io/status"
 
-# Use urllib to open the URL
 with urllib.request.urlopen(url) as response:
-    # Read the response
-    html = response.read()
-    # Print the response
+    body = response.read()
     print("Body response:")
-    print("\\t- type: {}".format(type(html)))
-    print("\\t- content: {}".format(html))
-    print("\\t- utf8 content: {}".format(html.decode('utf-8')))
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode('utf-8'))
