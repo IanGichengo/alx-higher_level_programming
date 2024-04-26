6e20e69d4df0963b11791f0b8bb0d2a77d99f3e3
IanGichengo <iangatimu6@gmail.com>
2024-04-26 10:36:21 -0400

    python_network_1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,11 +3,11 @@
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    response = requests.get(url)
+""" Get the URL from the command line arguments"""
+url = sys.argv[1]
+response = requests.get(url)
 
-    if response.status_code >= 400:
-        print("Error code:", response.status_code)
-    else:
-        print(response.text)
+if response.status_code >= 400:
+    print("Error code:", response.status_code)
+else:
+    print(response.text)
