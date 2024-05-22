d0c706c581c689ae8f570491eab05c75bec4ffa6
IanGichengo <iangatimu6@gmail.com>
2024-05-22 17:00:19 -0400

    web_scrapping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,19 +1,16 @@
 #!/usr/bin/node
-const fs = require('fs')
-
+const fs = require('fs');
 function readAndPrintFile (filePath) {
   try {
-    const content = fs.readFileSync(filePath, 'utf-8')
-    console.log(content)
+    const content = fs.readFileSync(filePath, 'utf-8');
+    console.log(content);
   } catch (error) {
-    console.error(`Error: ${error.message}`)
+    console.error(`Error: ${error.message}`);
   }
 }
-
 if (process.argv.length !== 3) {
-  console.error('Usage: node script_name.js <file_path>')
-  process.exit(1)
+  console.error('Usage: node script_name.js <file_path>');
+  process.exit(1);
 }
-
-const filePath = process.argv[2]
-readAndPrintFile(filePath)
+const filePath = process.argv[2];
+readAndPrintFile(filePath);
