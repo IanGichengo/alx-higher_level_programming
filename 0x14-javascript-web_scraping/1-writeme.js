40a08cfcb59216d6184cf3c3726b4511cca3385e
IanGichengo <iangatimu6@gmail.com>
2024-05-22 16:54:52 -0400

    web_scrapping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs')
-
 function writeToFile (filePath, content) {
   try {
     fs.writeFileSync(filePath, content, 'utf-8')
@@ -9,12 +7,10 @@ function writeToFile (filePath, content) {
     console.error(`Error: ${error.message}`)
   }
 }
-
 if (process.argv.length !== 4) {
   console.error('Usage: node script_name.js <file_path> <string_to_write>')
   process.exit(1)
 }
-
 const filePath = process.argv[2]
 const content = process.argv[3]
 writeToFile(filePath, content)
