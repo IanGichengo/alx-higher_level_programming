fd8226354b965c3588b0eed1a4d993ff8293fb3b
IanGichengo <iangatimu6@gmail.com>
2024-05-22 16:58:19 -0400

    web_scrapping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -11,6 +11,6 @@ if (process.argv.length !== 4) {
   console.error('Usage: node script_name.js <file_path> <string_to_write>');
   process.exit(1);
 }
-const filePath = process.argv[2;]
+const filePath = process.argv[2];
 const content = process.argv[3];
 writeToFile(filePath, content);
