e7e0389bfff63f048c1c170ec708ff376aae51ac
IanGichengo <iangatimu6@gmail.com>
2024-05-24 11:34:13 -0400

    web_scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,35 +1,16 @@
 #!/usr/bin/node
-'use strict';
-const request = require('request');
-if (process.argv.length !== 3) {
-  console.error('Usage: ./101-starwars_characters.js <Movie ID>');
-  process.exit(1);
+const axios = require('axios');
+async function getCharacterName (url) {
+  const response = await axios.get(url);
+  return response.data.name;
 }
-const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    process.exit(1);
+async function printCharacters (movieId) {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
+  const response = await axios.get(url);
+  const data = response.data;
+  for (const characterUrl of data.characters) {
+    const name = await getCharacterName(characterUrl);
+    console.log(name);
   }
-  if (response.statusCode !== 200) {
-    console.error('Failed to retrieve data. Status code:', response.statusCode);
-    process.exit(1);
-  }
-  const film = JSON.parse(body);
-  const characters = film.characters;
-  characters.forEach(characterUrl => {
-    request.get(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error:', charError);
-        return;
-      }
-      if (charResponse.statusCode !== 200) {
-        console.error('Failed to retrieve character. Status code:', charResponse.statusCode);
-        return;
-      }
-      const character = JSON.parse(charBody);
-      console.log(character.name);
-    });
-  });
-});
+}
+printCharacters('3');
